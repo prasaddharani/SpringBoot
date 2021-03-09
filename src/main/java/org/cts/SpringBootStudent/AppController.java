7ec83912b9ae3ca6cc8753a0c6bab9e29722ae92
Dharani Prasad <37272583+prasaddharani@users.noreply.github.com>
2021-03-09 20:07:44 +0530

    Update AppController.java

--- src/main/java/org/cts/SpringBootStudent/AppController.java
+++ src/main/java/org/cts/SpringBootStudent/AppController.java
@@ -2,9 +2,12 @@ package org.cts.SpringBootStudent;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,23 +22,27 @@ public class AppController {
 	
 	@RequestMapping("/")
 	public String viewHomePage(Model model) {
-		List<Student> listProducts = service.listAll();
-		model.addAttribute("listProducts", listProducts);
+		List<Student> listStudents = service.listAll();
+		model.addAttribute("listStudents", listStudents);
 		
 		return "index";
 	}
 	
 	@RequestMapping("/new")
-	public String showNewProductPage(Model model) {
-		Student product = new Student();
-		model.addAttribute("product", product);
+	public String showNewStudentPage(Model model) {
+		Student student = new Student();
+		model.addAttribute("student", student);
 		
 		return "new_student";
 	}
 	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public String saveProduct(@ModelAttribute("product") Student product) {
-		service.save(product);
+	public String saveProduct(@Valid @ModelAttribute("student") Student student,BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "new_student";
+		}
+		
+		service.save(student);
 		
 		return "redirect:/";
 	}
@@ -43,14 +50,14 @@ public class AppController {
 	@RequestMapping("/edit/{id}")
 	public ModelAndView showEditProductPage(@PathVariable(name = "id") int id) {
 		ModelAndView mav = new ModelAndView("edit_student");
-		Student product = service.get(id);
-		mav.addObject("product", product);
+		Student student = service.get(id);
+		mav.addObject("student", student);
 		
 		return mav;
 	}
 	
 	@RequestMapping("/delete/{id}")
-	public String deleteProduct(@PathVariable(name = "id") int id) {
+	public String deleteStudent(@PathVariable(name = "id") int id) {
 		service.delete(id);
 		return "redirect:/";		
 	}
